4d4558ecef710306d96b1734240f6f7ce328fb08
Ingridsegura <ingrid.segura@estudiantat.upc.edu>
2021-03-18 13:21:36 +0100

    entrega3.1

---
+++ Proyecto/BBDD.c
@@ -0,0 +1,74 @@
+//Incluir esta libreri\uffc2\uffada para poder hacer las llamadas en shiva2.upc.es
+//#include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	MYSQL *conn;
+	int err;
+	// Estructura especial para almacenar resultados de consultas 
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
+	
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexi\ufff3n: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexin
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "juego" ,0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	
+	char fecha[20];
+	// Pregunto la fecha de la partida
+	printf ("Dame una fecha separada por /\n"); 
+	scanf ("%s", fecha);
+	char hora[20];
+	// Pregunto la hora de la partida
+	printf ("Dame una hora separada por :\n"); 
+	scanf ("%s", hora);
+	//nos da el usuario que gano esa partida
+	char consulta [80];
+	sprintf (consulta,"SELECT JUGADOR.USUARIO FROM (JUGADOR,PARTIDAS)  WHERE PARTIDAS.FECHA = '%s' AND PARTIDAS.HORA = '%s'AND ID_G = JUGADOR.ID", fecha, hora);
+	
+	
+	err=mysql_query (conn, consulta);
+	if (err!=0) {
+		printf ("Error al consultar datos de la base %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//recogemos el resultado de la consulta. El resultado de la
+	//consulta se devuelve en una variable del tipo puntero a
+	//MYSQL_RES tal y como hemos declarado anteriormente.
+	//Se trata de una tabla virtual en memoria que es la copia
+	//de la tabla real en disco.
+	resultado = mysql_store_result (conn);
+	// El resultado es una estructura matricial en memoria
+	// en la que cada fila contiene los datos de una persona.
+	
+	// Ahora obtenemos la primera fila que se almacena en una
+	// variable de tipo MYSQL_ROW
+	row = mysql_fetch_row (resultado);
+	
+	if (row == NULL)
+		printf ("No se han obtenido datos en la consulta\n");
+	else
+		while (row !=NULL) {
+			// la columna 0 contiene el nombre del jugador
+			printf ("%s\n", row[0]);
+			// obtenemos la siguiente fila
+			row = mysql_fetch_row (resultado);
+	}
+		
+		mysql_close (conn);
+		exit(0);
+}
